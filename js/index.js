6307cef68374c8e91af1cade0c2a0c7236ad271e
KarolCifuentes <karolsofi16@gmail.com>
2023-06-20 23:42:40 -0500

    clase 1 react

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const container = document.getElementById('container');
+const button = document.createElement('button');
+
+button.innerText = 'Click me';
+button.id = 'home-button';
+button.className = 'primary-button';
+
+container.appendChild(button);
